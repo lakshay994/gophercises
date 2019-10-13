7ed196bba6287116b087f194f83f31e013d9717e
lakshay <lakshay.sharma994@gmail.com>
2019-10-13 18:08:25 -0500

    Part 2 of the problem solved.

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -5,15 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	csvFile := flag.String("csv", "", ".csv file for creating the quiz")
+	timeDuration := flag.Int("duration", 30, "time duration for the quiz")
 	flag.Parse()
-	generateQuiz(csvFile)
+	generateQuiz(csvFile, timeDuration)
 }
 
-func generateQuiz(filename *string) {
+func generateQuiz(filename *string, duration *int) {
 	file, err := os.Open(*filename)
 	if err != nil {
 		exit("No such file found")
@@ -27,13 +29,28 @@ func generateQuiz(filename *string) {
 
 	problems := parseFileContent(content)
 
+	timer := time.NewTimer(time.Duration(*duration) * time.Second)
+
 	correctAnswers := 0
 	for i, problem := range problems {
+
 		fmt.Printf("Problem %d. %s \n", i+1, problem.question)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == problem.answer {
-			correctAnswers++
+
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nTimes Up! \nYou got %d out of %d answers right\n", correctAnswers, len(problems))
+			os.Exit(1)
+		case answer := <-answerCh:
+			if answer == problem.answer {
+				correctAnswers++
+			}
 		}
 	}
 
